f60f875694f5d1efe9c34164d10e867807f02bc0
Caleb <Tigers1025@gmail.com>
2022-08-11 16:26:02 -0400

    commit tree 3f51fc22e0ca846b12f250fc36025f1933c1d8d2

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+let thankYouMessages = [];
+
+function writeCards(name) {
+for (let i = 0; i < name.length; i++ ){
+   thankYouMessages.push( `Thank you, ${name[i]}, for the wonderful surprise gift!`);
+}
+return thankYouMessages
+}
+
+
+
+function countDown(number){
+    let counter = number
+    while (counter >= 0){
+        console.log(counter);
+        counter--
+    }
+        return 
+}
+
+countDown(10)
+
